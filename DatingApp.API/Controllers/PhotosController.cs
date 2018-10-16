48614001dd1c64b61b85bb3c291dbfd1bbeeb020
Venky Venkataraman <venky76v@gmail.com>
2018-10-17 00:04:33 +0100

    Added new Cloudinary config for photos upload, configuration settings.

---
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using CloudinaryDotNet;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.API.Controllers {
+    [Authorize]
+    [Route ("api/users/{userId}/photos")]
+    [ApiController]
+    public class PhotosController : ControllerBase {
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+        private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+        private Cloudinary _cloudinary;
+
+        public PhotosController (IDatingRepository repo, IMapper mapper, 
+            IOptions<CloudinarySettings> cloudinaryConfig) {
+            _cloudinaryConfig = cloudinaryConfig;
+            _mapper = mapper;
+            _repo = repo;
+
+            Account acc = new Account(
+                _cloudinaryConfig.Value.CloudName,
+                _cloudinaryConfig.Value.ApiKey,
+                _cloudinaryConfig.Value.ApiSecret
+            );
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto) {
+            return Ok(new NotImplementedException());
+        }
+    }
+}
